d75104f92aec04ab138baca544a4849288fa830d
IsmaelCesar <leamscesar@gmail.com>
2017-10-29 13:05:49 -0300

    Formatação Tela Login
    Formatação da tela de login do app

--- Guia-BCC/app/src/main/java/com/ufrpe/bcc/guia_bcc/MainActivity.java
+++ Guia-BCC/app/src/main/java/com/ufrpe/bcc/guia_bcc/MainActivity.java
@@ -2,12 +2,52 @@ package com.ufrpe.bcc.guia_bcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtNomeUsuario;
+    EditText edtSenha;
+    Button btnEntrar;
+    Button btnEsqueciSenha;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtNomeUsuario = (EditText) findViewById(R.id.edtNome);
+        edtNomeUsuario = (EditText) findViewById(R.id.edtSenha);
+
+        if(savedInstanceState != null){
+            /*Os seguintes testes evitam null pointer exception na hora de  setar os textos*/
+            if(savedInstanceState.getString("nome") != null)
+                edtNomeUsuario.setText(savedInstanceState.getString("nome"));
+            if(savedInstanceState.getString("senha") != null)
+                edtSenha.setText(savedInstanceState.getString("senha"));
+        }
+
+
+
+
+    }
+
+    //Salvando o estado da aplicacaos
+    @Override
+    public void onSaveInstanceState(Bundle b){
+        super.onSaveInstanceState(b);
+
+        if(edtNomeUsuario != null){
+            if(edtNomeUsuario.getText().toString() != null)
+                b.putString("nome",edtNomeUsuario.getText().toString());
+        }
+        if(edtSenha != null){
+            if(edtSenha.getText().toString() != null)
+                b.putString("senha",edtSenha.getText().toString());
+        }
+
     }
 }
